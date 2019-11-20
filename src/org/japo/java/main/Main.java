6aa474fc7ba358bb9189424ff3bda7cd5e4e3114
Manu-P-Z <55140316+Manu-P-Z@users.noreply.github.com>
2019-11-20 20:53:54 +0100

    :tada: Commit Inicial 03303-CuentaProgresivaFor

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,43 @@
+/* 
+ * Copyright 2019 Manu Portolés Zagalá.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Manu Portolés Zagalá
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        
+        final int CUENTA_INI = 0;
+        final int CUENTA_FIN = 10;
+
+        int i;
+
+        
+        
+        for (i = CUENTA_INI; i < CUENTA_FIN; i++) {
+            System.out.println(i);
+    }
+            
+    }
+
+}
